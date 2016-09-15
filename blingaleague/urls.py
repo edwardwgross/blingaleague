bec1070dfeaf6c312660190081130cd7575fb665
Edward W. Gross <edward.w.gross@gmail.com>
2016-09-15 03:44:55 +0000

    Update urls

--- blingaleague/urls.py
+++ blingaleague/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from .views import Home, Standings
 
 urlpatterns = patterns('',
-    url(r'^/$', Home.as_view(), name='blingaleague.home'),
+    url(r'^$', Home.as_view(), name='blingaleague.home'),
     url(r'^standings/(?P<year>)\d+/$', Standings.as_view(), name='blingaleague.standings'),
 )
