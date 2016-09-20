9daac78b9c57fa2f3cdaf34d11a4d781b51f2176
Edward W. Gross <edward.w.gross@gmail.com>
2016-09-20 23:56:25 +0000

    Added games view

--- blingaleague/urls.py
+++ blingaleague/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import HomeView, StandingsView, TeamVsTeamView, AddGameResultView
+from .views import HomeView, StandingsView, GamesView, TeamVsTeamView, AddGameResultView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='blingaleague.home'),
     url(r'^standings/$', StandingsView.as_view(), name='blingaleague.standings'),
+    url(r'^games/$', GamesView.as_view(), name='blingaleague.games'),
     url(r'^team_vs_team/$', TeamVsTeamView.as_view(), name='blingaleague.team_vs_team'),
     url(r'^add_game_result/$', AddGameResultView.as_view(), name='blingaleague.add_game_result'),
 )
--- blingaleague/views.py
+++ blingaleague/views.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 
 from django.core import urlresolvers
+from django.db.models import Q
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 
-from .models import Standings, Game
+from .models import Standings, Game, Member
 
 class HomeView(TemplateView):
     template_name = 'blingaleague/home.html'
@@ -57,6 +58,53 @@ class StandingsView(TemplateView):
 
 
         context = {'standings': standings, 'links': links}
+
+        return self.render_to_response(context)
+
+
+class GamesView(TemplateView):
+    template_name = 'blingaleague/games.html'
+
+    def get(self, request):
+        games = Game.objects.all()
+
+        headline = 'Games'
+
+        teams = [Member.objects.get(pk=team_id) for team_id in request.GET.getlist('team')]
+        teams = sorted(teams, key=lambda x: x.full_name)[:2]  # never want more than 2
+
+        years = [int(year) for year in request.GET.getlist('year')]
+        weeks = [int(week) for week in request.GET.getlist('week')]
+
+        for team in teams:
+            games = games.filter(Q(winner=team) | Q(loser=team))
+
+        if len(teams) > 1:
+            # we're doing a matchup, so don't allow any date restrictions
+            headline = "%s played between %s and %s" % (headline, teams[0], teams[1])
+        else:
+            if teams:
+                if years:
+                    games = games.filter(year__in=years)
+                if weeks:
+                    games = games.filter(week__in=weeks)
+            else:
+                # if we didn't give a team filter, limit to the most recent one year and week specified
+                if years:
+                    year = years[-1]
+                else:
+                    year = Game.objects.all().order_by('-year').values_list('year', flat=True)[0]
+
+                if weeks:
+                    week = weeks[-1]
+                else:
+                    week = Game.objects.filter(year=year).order_by('-week').values_list('week', flat=True)[0]
+
+                games = games.filter(year=year, week=week)
+
+        games = games.order_by('year', 'week', 'winner_score', 'loser_score')
+
+        context = {'games': games}
 
         return self.render_to_response(context)
 
