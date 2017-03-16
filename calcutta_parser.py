0a0d6c394c825fc3c4301c2f97adda08c16a4e4d
Edward W. Gross <edward.w.gross@gmail.com>
2017-03-16 15:45:54 +0000

    Way to quickly parse calcutta payments

---
+++ calcutta_parser.py
@@ -0,0 +1,19 @@
+import decimal
+import sys
+
+output_lines = []
+
+for line in sys.stdin.readlines():
+    line = line.strip()
+
+    team, price, buyers = line.split('|')
+
+    buyer_list = [b.strip() for b in buyers.split(',')]
+
+    buyer_share = "=1/%i" % len(buyer_list)#decimal.Decimal(1) / decimal.Decimal(len(buyer_list))
+
+    for buyer in buyer_list:
+        output_lines.append(','.join((buyer, team, price, buyer_share)))
+
+for line in sorted(output_lines):
+    print line
