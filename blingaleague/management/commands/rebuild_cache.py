190044c2d637670f6f94c55392a2d7100384efaa
Edward W. Gross <edward.w.gross@gmail.com>
2017-10-18 02:49:54 +0000

    Remove CPU intensive job

--- blingaleague/management/commands/rebuild_cache.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from blingaleague.models import rebuild_whole_cache
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        rebuild_whole_cache()
