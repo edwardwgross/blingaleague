2d9f186e97faa063c554beca60b070c7fcc9b816
Edward W. Gross <edward.w.gross@gmail.com>
2018-06-23 03:27:03 +0000

    FLAKE8: blingaltics urls

--- blingalytics/urls.py
+++ blingalytics/urls.py
@@ -6,8 +6,24 @@ from .views import WeeklyScoresView, ExpectedWinsView, GameFinderView, SeasonFin
 default_cache_timeout = 365 * 24 * 60 * 60
 
 urlpatterns = [
-    url(r'weekly_scores/$', cache_page(default_cache_timeout)(WeeklyScoresView.as_view()), name='blingalytics.weekly_scores'),
-    url(r'expected_wins/$', cache_page(default_cache_timeout)(ExpectedWinsView.as_view()), name='blingalytics.expected_wins'),
-    url(r'game_finder/$', cache_page(default_cache_timeout)(GameFinderView.as_view()), name='blingalytics.game_finder'),
-    url(r'season_finder/$', cache_page(default_cache_timeout)(SeasonFinderView.as_view()), name='blingalytics.season_finder'),
+    url(
+        r'weekly_scores/$',
+        cache_page(default_cache_timeout)(WeeklyScoresView.as_view()),
+        name='blingalytics.weekly_scores',
+    ),
+    url(
+        r'expected_wins/$',
+        cache_page(default_cache_timeout)(ExpectedWinsView.as_view()),
+        name='blingalytics.expected_wins',
+    ),
+    url(
+        r'game_finder/$',
+        cache_page(default_cache_timeout)(GameFinderView.as_view()),
+        name='blingalytics.game_finder',
+    ),
+    url(
+        r'season_finder/$',
+        cache_page(default_cache_timeout)(SeasonFinderView.as_view()),
+        name='blingalytics.season_finder',
+    ),
 ]
