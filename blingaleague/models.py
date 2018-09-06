cce4a843b20e62607b60944b257072166e13d9ab
Edward W. Gross <edward.w.gross@gmail.com>
2018-09-06 19:17:49 +0000

    Handle weeks without games

--- blingaleague/models.py
+++ blingaleague/models.py
@@ -976,6 +976,9 @@ class Week(object):
 
     @fully_cached_property
     def headline(self):
+        if len(self.games) == 0:
+            return 'No games yet'
+
         if self.week > REGULAR_SEASON_WEEKS:
             return self.games[0].title
 
