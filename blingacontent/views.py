7d518d595db769d9b9fc5601e9a2d73d55c2d460
Edward W. Gross <edward.w.gross@gmail.com>
2018-09-26 21:57:17 +0000

    Improve searching for gazettes

--- blingacontent/views.py
+++ blingacontent/views.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.views.generic import ListView, DetailView
 
 from .forms import GazetteSearchForm
@@ -9,14 +11,23 @@ class GazetteListView(ListView):
     form_class = GazetteSearchForm
     context_object_name = 'gazette_list'
 
+    def parse_full_search_term(self):
+        full_search_term = self.form.cleaned_data['search']
+        parsed_terms = list(csv.reader([full_search_term], delimiter=' '))[0]
+        return filter(lambda x: x, parsed_terms)
+
     def get_queryset(self):
         queryset = Gazette.objects.filter(publish_flag=True)
 
         self.form = self.form_class(self.request.GET)
         if self.form.is_valid():
-            queryset = queryset.filter(
-                body__icontains=self.form.cleaned_data['search']
-            )
+            full_filter = None
+
+            for search_part in self.parse_full_search_term():
+                if not search_part:
+                    continue  # take care of extra spaces
+
+                queryset = queryset.filter(body__icontains=search_part)
 
         queryset = queryset.order_by('-published_date', 'headline')
 
