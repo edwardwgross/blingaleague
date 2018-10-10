038617f68a97418cf60063e1017b5399cc0c663d
Edward W. Gross <edward.w.gross@gmail.com>
2018-10-10 16:00:30 +0000

    Make django admin UI a little better

--- blingacontent/admin.py
+++ blingacontent/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import Textarea
 
 from .models import Meme, Gazette
 
 
 class GazetteAdmin(admin.ModelAdmin):
     readony_fields = ('slug_url',)
+
+    formfield_overrides = {
+        models.TextField: {
+            'widget': Textarea(attrs={'rows': 30, 'cols': 100}),
+        }
+    }
 
 
 admin.site.register(Meme)
--- blingaleague/admin.py
+++ blingaleague/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import NumberInput
 
 from .models import Member, Game, Season, FakeMember
 
 
+class GameAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.IntegerField: {
+            'widget': NumberInput(attrs={'type': 'number'}),
+        }
+    }
+
+
 admin.site.register(Member)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Season)
 admin.site.register(FakeMember)
