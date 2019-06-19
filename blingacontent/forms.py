0c5de12bc84a85fb662a4c629923fc13fef3d9ab
Edward W. Gross <edward.w.gross@gmail.com>
2019-06-19 15:00:21 +0000

    Put the tags on the form on the gazette page, update styles

--- blingacontent/forms.py
+++ blingacontent/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from tagging.forms import TagField
+
 
 class GazetteSearchForm(forms.Form):
     search = forms.CharField(required=False, label='Search term')
+    tag = TagField(required=False, label='Tags')
--- blingacontent/views.py
+++ blingacontent/views.py
@@ -3,6 +3,7 @@ import csv
 from django.views.generic import ListView, DetailView
 
 from tagging.models import TaggedItem
+from tagging.utils import parse_tag_input
 
 from .forms import GazetteSearchForm
 from .models import Gazette
@@ -13,50 +14,57 @@ class GazetteListView(ListView):
     form_class = GazetteSearchForm
     context_object_name = 'gazette_list'
 
+    def parse_full_tag_input(self):
+        full_tag_input = self.form.cleaned_data['tag']
+        return parse_tag_input(full_tag_input)
+
     def parse_full_search_term(self):
         full_search_term = self.form.cleaned_data['search']
-        parsed_terms = list(csv.reader([full_search_term], delimiter=' '))[0]
-        return filter(lambda x: x, parsed_terms)
+        return parse_tag_input(full_search_term)
 
     def get_queryset(self):
-        queryset = Gazette.objects.filter(publish_flag=True)
-
-        self.tags = sorted(self.request.GET.getlist('tag'))
-        if self.tags:
-            all_tagged_ids = None
-
-            for tag in self.tags:
-                tagged_ids = set(TaggedItem.objects.filter(
-                    tag__name=tag,
-                ).values_list('object_id', flat=True))
-
-                if all_tagged_ids is None:
-                    all_tagged_ids = tagged_ids
-                else:
-                    all_tagged_ids.intersection_update(tagged_ids)
-
-            if all_tagged_ids:
-                queryset = queryset.filter(pk__in=all_tagged_ids)
-            else:
-                # if nothing is tagged, force no results
-                return queryset.none()
+        queryset = Gazette.objects.filter(
+            publish_flag=True,
+        ).order_by(
+            '-published_date',
+            'headline',
+        )
 
         self.form = self.form_class(self.request.GET)
+
         if self.form.is_valid():
-            for search_part in self.parse_full_search_term():
-                if not search_part:
+            tags = self.parse_full_tag_input()
+            if tags:
+                all_tagged_ids = None
+
+                for tag in tags:
+                    tagged_ids = set(TaggedItem.objects.filter(
+                        tag__name=tag,
+                    ).values_list('object_id', flat=True))
+
+                    if all_tagged_ids is None:
+                        all_tagged_ids = tagged_ids
+                    else:
+                        all_tagged_ids.intersection_update(tagged_ids)
+
+                if all_tagged_ids:
+                    queryset = queryset.filter(pk__in=all_tagged_ids)
+                else:
+                    # if nothing is tagged, force no results
+                    return queryset.none()
+
+            search_terms = self.parse_full_search_term()
+            for search_term in search_terms:
+                if not search_term:
                     continue  # take care of extra spaces
 
-                queryset = queryset.filter(body__icontains=search_part)
-
-        queryset = queryset.order_by('-published_date', 'headline')
+                queryset = queryset.filter(body__icontains=search_term)
 
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['form'] = self.form
-        context['tags'] = self.tags
         return context
 
 
