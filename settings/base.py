00d2e4caaec55422eadf560e668bab3094758e6c
Edward W. Gross <edward.w.gross@gmail.com>
2024-10-23 18:17:49 +0000

    Increase default MAX_ENTRIES for caches (is this the problem?)

--- settings/base.py
+++ settings/base.py
@@ -97,12 +97,18 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': None,
         'KEY_PREFIX': 'default-',
+        'OPTIONS': {
+            'MAX_ENTRIES': 3000,
+        },
     },
     'blingaleague': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': None,
         'KEY_PREFIX': 'blingaleague-',
+        'OPTIONS': {
+            'MAX_ENTRIES': 3000,
+        },
     },
 }
 
