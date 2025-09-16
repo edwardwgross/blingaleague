0632f107e2a7fe9322ad0f0447902e9bea214b7b
Edward W. Gross <edward.w.gross@gmail.com>
2025-09-16 15:23:30 +0000

    Add management command to clear playoff odds jobs

---
+++ blingalytics/management/commands/clear_playoff_odds_queue.py
@@ -0,0 +1,15 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from .utils import CACHE, PLAYOFF_ODDS_QUEUE_CACHE_KEY, \
+                   PLAYOFF_ODDS_ACTIVELY_RUNNING_CACHE_KEY
+
+
+class Command(BaseCommand):
+
+    label = 'caches_to_clear'
+
+    def handle(self, *args, **kwargs):
+        CACHE.delete(PLAYOFF_ODDS_QUEUE_CACHE_KEY)
+        CACHE.delete(PLAYOFF_ODDS_ACTIVELY_RUNNING_CACHE_KEY)
