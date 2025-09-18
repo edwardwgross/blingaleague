dac1cc708e1fd9f7d8b9de208d19e9c33a6fb68d
Edward W. Gross <edward.w.gross@gmail.com>
2025-09-18 20:14:25 +0000

    Add start and end time

--- scripts/pre_build_cache.sh
+++ scripts/pre_build_cache.sh
@@ -4,6 +4,8 @@ BASE_DIR=/data/blingaleague
 PYTHON=$BASE_DIR/environ/bin/python
 LOG_FILE=$BASE_DIR/logs/pre_build_cache.log
 
-echo "[`date`]" > $LOG_FILE
+echo "STARTED: `date`" > $LOG_FILE
 
 $PYTHON $BASE_DIR/manage.py pre_build_cache >> $LOG_FILE
+
+echo "ENDED: `date`" >> $LOG_FILE
